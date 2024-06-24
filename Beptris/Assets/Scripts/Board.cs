a53be53e12bedaf0e6d0dbade450c74a63579451
Wigglybananas <146724073+Wigglybananas@users.noreply.github.com>
2024-06-24 12:36:07 +0100

    Bap 15

--- Beptris/Assets/Scripts/Board.cs
+++ Beptris/Assets/Scripts/Board.cs
@@ -5,12 +5,25 @@ using UnityEngine.Tilemaps;
 
 public class Board : MonoBehaviour
 {
-    public Tilemap tilemap {  get; private set; }
+    public Tilemap tilemap { get; private set; }
+    public Piece activePiece { get; private set; }
     public BeptrimoData[] beptrimos;
+    public Vector3Int spawnPostition;
+    public Vector2Int boardSize = new Vector2Int(10, 20);
+
+    public RectInt Bounds
+    {
+        get
+        {
+            Vector2Int position = new Vector2Int(-this.boardSize.x / 2, -this.boardSize.y / 2);
+            return new RectInt(position, this.boardSize);
+        }
+    }
 
     private void Awake()
     {
         this.tilemap = GetComponentInChildren<Tilemap>();
+        this.activePiece = GetComponentInChildren<Piece>();
         
         for (int i = 0; i < this.beptrimos.Length; i++)
         {
@@ -27,10 +40,47 @@ public class Board : MonoBehaviour
     {
         int random = Random.Range(0, this.beptrimos.Length);
         BeptrimoData data = this.beptrimos[random];
+
+        this.activePiece.Initialize(this, this.spawnPostition, data);
+        Set(this.activePiece);
     }
 
-    public void Set()
+    public void Set(Piece piece)
     {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            this.tilemap.SetTile(tilePosition, piece.data.tile);
+        }
+    }
 
+    public void Clear(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            this.tilemap.SetTile(tilePosition, null);
+        }
+    }
+
+    public bool IsValidPosition(Piece piece, Vector3Int position)
+    {
+        RectInt bounds = this.Bounds;
+
+        for(int i = 0;i < piece.cells.Length;i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + position;
+
+            if (!bounds.Contains((Vector2Int)tilePosition))
+            {
+                return false;
+            }
+
+            if (this.tilemap.HasTile(tilePosition))
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- Beptris/Assets/Scripts/Piece.cs
+++ Beptris/Assets/Scripts/Piece.cs
@@ -6,10 +6,125 @@ public class Piece : MonoBehaviour
 {
     public Board board {  get; private set; }
     public BeptrimoData data { get; private set; }
+    public Vector3Int[] cells { get; private set; }
     public Vector3Int position { get; private set; }
+    public int rotationIndex { get; private set; }
     
     public void Initialize(Board board, Vector3Int postion, BeptrimoData data)
     {
+        this.board = board;
+        this.position = postion;
+        this.data = data;
+        this.rotationIndex = 0;
 
+        if (this.cells == null )
+        {
+            this.cells = new Vector3Int[data.cells.Length];
+        }
+
+        for ( int i = 0; i < this.cells.Length; i++ )
+        {
+            this.cells[i] = (Vector3Int)data.cells[i];
+        }
+    }
+
+    private void Update()
+    {
+        this.board.Clear(this);
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Rotate(-1);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            Rotate(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Move(Vector2Int.left);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Move(Vector2Int.right);
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Move(Vector2Int.down);
+        }
+
+        if ( Input.GetKeyDown(KeyCode.Space))
+        {
+            HardDrop();
+        }
+
+        this.board.Set(this);
+    }
+
+    private void HardDrop()
+    {
+        while (Move(Vector2Int.down))
+        {
+            continue;
+        }
+    }
+
+    private bool Move(Vector2Int translation)
+    {
+        Vector3Int newPosition = this.position;
+        newPosition.x += translation.x;
+        newPosition.y += translation.y;
+
+        bool valid = this.board.IsValidPosition(this, newPosition);
+
+        if (valid)
+        {
+            this.position = newPosition;
+        }
+        return valid;
+    }
+
+    private void Rotate(int direction)
+    {
+        this.rotationIndex = Wrap(this.rotationIndex + direction, 0, 4);
+
+        for (int i = 0; i < this.cells.Length; i++)
+        {
+            Vector3 cell = this.cells[i];
+
+            int x, y;
+
+            switch (this.data.beptrimo)
+            {
+                case Beptrimo.I:
+                case Beptrimo.O:
+                    cell.x -= 0.5f;
+                    cell.y -= 0.5f;
+                    x = Mathf.CeilToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
+                    y = Mathf.CeilToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
+                    break;
+
+                default:
+                    x = Mathf.RoundToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
+                    y = Mathf.RoundToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
+                    break;
+            }
+
+            this.cells[i] = new Vector3Int(x, y, 0);
+        }
+    }
+
+    private int Wrap(int input, int min, int max)
+    {
+        if (input < min)
+        {
+            return max - (min - input) % (max - min);
+        }
+        else
+        {
+            return min + (input - min) % (max - min);
+        }
     }
 }
